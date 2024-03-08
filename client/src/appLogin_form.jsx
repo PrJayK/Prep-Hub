535161acac580bc6ee47653dd244eac223c9af11
RAGHAV <119474871+RAGHAVS1304@users.noreply.github.com>
2024-03-08 22:12:40 +0530

    Create appLogin_form.jsx

---
+++ client/src/appLogin_form.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import Login_signup from './Components/LoginSignup/Login_signup.jsx';
+
+function App() {
+  return (
+    <div >
+     <Login_signup/>
+    </div>
+  );
+}
+
+export default App;
