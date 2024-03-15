4f8be16271d038a2b538a1558a035c4ab8ec1854
RAGHAV <119474871+RAGHAVS1304@users.noreply.github.com>
2024-03-15 15:10:50 +0530

    Create data.js

---
+++ client/src/Add_subject/data.js
@@ -0,0 +1,56 @@
+import subject from "../assets/chembg.jpg";
+const data=[
+    {   
+        id:"",
+        name:"Physics",
+        image:""
+
+    },
+    {
+        id:"",
+        name:"Chemistry",
+        image:subject
+    },
+    {
+        id:"",
+        name:"Maths",
+        image:""
+    },
+    {
+        id:"",
+        name:"Physics",
+        image:""
+    },
+    {
+        id:"",
+        name:"Chemistry",
+        image:subject
+    },
+    {
+        id:"",
+        name:"Maths",
+        image:""
+    },
+    {
+        id:"",
+        name:"Physics",
+        image:""
+    },
+    {
+        id:"",
+        name:"Maths",
+        image:""
+    },
+    {
+        id:"",
+        name:"Physics",
+        image:""
+    },
+    {
+        id:"",
+        name:"Chemistry",
+        image:subject
+    },
+
+]
+export default data;
